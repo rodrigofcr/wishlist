f7103d6399163db082d632b544e2719d5368c7e7
Rodrigo Reis <rodrigo_fcr@hotmail.com>
2024-04-21 11:43:21 -0300

    Base project init

---
+++ src/main/java/com/github/rodrigofcr/whishlist/WhishlistApplication.java
@@ -0,0 +1,13 @@
+package com.github.rodrigofcr.whishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhishlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rodrigofcr/whishlist/WhishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rodrigofcr.whishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
